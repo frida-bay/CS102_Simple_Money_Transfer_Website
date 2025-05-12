173f7bc9aac3179f09925b8a36dc0d48c5065d36
Farida <fridabayramli@gmail.com>
2025-05-12 16:03:26 +0400

    server.js file updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // API: LOGIN
 app.post('/api/login', (req, res) => {
